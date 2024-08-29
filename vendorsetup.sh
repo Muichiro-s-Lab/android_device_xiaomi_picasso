e3b3f91de4dbb9df37dc814d2faf9da5c3cf5761
Muichiro Tokito <132120605+Muichiro09@users.noreply.github.com>
2024-08-29 14:06:51 +0800

    picasso: Add vendorsetup script

---
+++ vendorsetup.sh
@@ -0,0 +1,15 @@
+# kernel/xiaomi
+rm -rf kernel/xiaomi
+git clone -b android-4.19 https://github.com/Muichiro-Lab/kernel_xiaomi_sm7250.git kernel/xiaomi/sm7250 --depth=1 --recursive
+
+# vendor/xiaomi/sm8250-common
+rm -rf vendor/xiaomi/sm8250-common
+git clone -b lineage-21.0 https://github.com/Muichiro-Lab/android_vendor_xiaomi_sm8250-common.git vendor/xiaomi/sm8250-common --depth=1
+
+# vendor/xiaomi
+rm -rf vendor/xiaomi/picasso
+git clone -b lineage-21.0 https://github.com/Muichiro-Lab/android_vendor_xiaomi_picasso.git vendor/xiaomi/picasso --depth=1
+
+# device/xiaomi/sm8250-common
+rm -rf device/xiaomi/sm8250-common
+git clone -b 1 https://github.com/Muichiro-s-Lab/android_device_xiaomi_sm8250-common.git device/xiaomi/sm8250-common --depth=1
